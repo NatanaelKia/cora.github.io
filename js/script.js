1adbbbe313880e53a79258d12a0b81518400d291
NatanaelKia <149179291+NatanaelKia@users.noreply.github.com>
2023-11-07 10:48:36 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -46,9 +46,6 @@ ScrollReveal({
     delay: 200
 });
 
-    
-
-
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
